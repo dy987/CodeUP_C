9eb9d8bb5cc862365081b072d3b09cad3c542423
yspark <pdy910626@gmail.com>
2021-03-01 23:35:41 +0900

    1089 까지 문제 품

--- Code_UP_C++/1085/1085.cpp
+++ Code_UP_C++/1085/1085.cpp
@@ -1,20 +1,26 @@
-﻿// 1085.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿//https://codeup.kr/problemsetsol.php?psid=23
+//1085 : [기초-종합] 소리 파일 저장용량 계산하기(설명)
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <stdio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	unsigned int h;
+	unsigned int b;
+	unsigned int c;
+	unsigned int s;
+	long long int sum = 0;
+	scanf("%d", &h);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	scanf("%d", &s);
+
+	sum = h * b * c * s / 8 / 1024;
+
+	printf("%0.1f MB",(float)(sum/1024.0));
+
+
+	return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
