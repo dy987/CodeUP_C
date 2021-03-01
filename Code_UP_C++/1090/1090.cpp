00cdc93971e260003816e428fb6bc965a762afb7
yspark <pdy910626@gmail.com>
2021-03-01 23:45:56 +0900

    1099 까지 빈 프로젝트 추가

---
+++ Code_UP_C++/1090/1090.cpp
@@ -0,0 +1,25 @@
+﻿//https://codeup.kr/problemsetsol.php?psid=23
+//1090 : [기초-종합] 수 나열하기2
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int s;
+	int n;
+	int c;
+	scanf("%d", &s);
+	scanf("%d", &n);
+	scanf("%d", &c);
+	long long int nc = 1;
+	for (int i = 0; i < c - 1; i++)
+		nc *= n;
+
+	printf("%lld ", s* nc);
+
+
+
+	return 0;
+}
---
+++ Code_UP_C++/1091/1091.cpp
@@ -0,0 +1,29 @@
+﻿//https://codeup.kr/problemsetsol.php?psid=23
+//1091 : [기초-종합] 수 나열하기3
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int m;
+	int d;
+	int n;
+
+	scanf("%d", &n);
+	scanf("%d", &a);
+	scanf("%d", &m);
+	scanf("%d", &d);
+	int array[100] = { a, };
+
+	for (int i = 1; i < 100; i++)
+		array[i] = array[i - 1]*m+d;
+	
+	printf("%d",array[n]);
+
+
+
+	return 0;
+}
