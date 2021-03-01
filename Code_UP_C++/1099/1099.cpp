9319b7bae823f5a98fdfd7b2de4cb6db8217f6bb
yspark <pdy910626@gmail.com>
2021-03-02 00:49:38 +0900

    C++ 기본 100제  - 1099 문제 까지 풀이 진행

--- Code_UP_C++/1099/1099.cpp
+++ Code_UP_C++/1099/1099.cpp
@@ -10,32 +10,51 @@ int main()
 	int i, j;
 	int a[11][11] = { 0, };
 	int b[11][11] = { 0, };
+	int x = 2;
+	int y = 2;
 	for (i = 1; i <= 10; i++) //한 줄씩 바둑판 상황 입력 받기
 		for (j = 1; j <= 10; j++) {
 			scanf("%d", &a[i][j]);
-			scanf("%d", &b[i][j]);
+			b[i][j] = a[i][j];
 		}
+	b[y][x] = 9;
 
-	for (i = 1; i <= n; i++) //좌표의 개수만큼
-	{
-		for (j = 1; j <= 19; j++) //가로 줄 흑<->백 바꾸기
+	if (a[y][x] == 2) {
+		
+	}
+	else {
+		for (i = 2; i <= 120; i++) //좌표의 개수만큼
 		{
-			if (a[x][j] == 0) a[x][j] = 1;
-			else a[x][j] = 0;
-		}
-		for (j = 1; j <= 19; j++) //세로 줄 흑<->백 바꾸기
-		{
-			if (a[j][y] == 0) a[j][y] = 1;
-			else a[j][y] = 0;
+			if (a[y][x + 1] == 0) {
+				b[y][x + 1] = 9;
+				x += 1;
+			}else if (a[y][x + 1] == 2) {
+				b[y][x + 1] = 9;
+				x += 1;
+				break;
+			}
+			else if (a[y][x + 1] == 1) {
+				if (a[y + 1][x] == 0) {
+					b[y + 1][x] = 9;
+					y += 1;
+				}
+				else if (a[y + 1][x] == 2) {
+					b[y + 1][x] = 9;
+					y += 1;
+					break;
+				}
+			}
+
 		}
 	}
 
-	for (i = 1; i <= 19; i++) { //한 줄씩 바둑판 상황 입력 받기
-		for (j = 1; j <= 19; j++) {
-			printf("%d ", a[i][j]);
+
+	
+	for (i = 1; i <= 10; i++) {
+		for (j = 1; j <= 10; j++) {
+			printf("%d ", b[i][j]);
 		}
 		printf("\n");
 	}
-
 	return 0;
 }
